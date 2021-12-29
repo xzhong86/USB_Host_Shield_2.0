4937bcee6270311122dd377d20106ee83807fc15
BruceZh <xzhong86@163.com>
2021-12-29 17:42:21 +0800

    update cmakefile and rp2040-pins.h

--- rp2040-pins.h
+++ rp2040-pins.h
@@ -2,6 +2,8 @@
 #ifndef __rp2040_pins__
 #define __rp2040_pins__
 
+#ifndef RP2040_PINS_USER
+
 #define PIN_SPI_SCK   10
 #define PIN_SPI_MOSI  11
 #define PIN_SPI_MISO  12
@@ -10,5 +12,10 @@
 
 #define MAX3421E_SS   P13
 #define MAX3421E_INTR P14
+
+#else // defined RP2040_PINS_USER
+#include "rp2040-pins-user.h"
+
+#endif
 
 #endif
